df666f45b426e10585b40fca169cd924c0ea2dc4
Sebastian Töllke <stollke@techstyle.com>
2024-08-28 13:45:38 +0100

    dding logger

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,26 @@
 import { Hono } from 'hono'
+import { logger } from 'hono/logger'
 import { renderer } from './renderer'
 
 const app = new Hono()
 
+export const customLogger = (message: string, ...rest: string[]) => {
+  console.log(message, ...rest)
+}
+
 app.use(renderer)
 
+app.use(logger(customLogger))
+
 app.get('/', (c) => {
+  const domainArray = c.req.url.split('.');
+  console.log(domainArray)
+  // customLogger('incoming connection', JSON.stringify(c))
   return c.render(
     <section>
       <h1>sbstn-sh</h1>
     </section>,
-    { title: 'home' }
+    { title: 'command not found: www' }
   )
 })
 
